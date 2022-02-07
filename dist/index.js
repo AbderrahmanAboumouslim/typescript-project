b0aa6deb5dc81bd7d7b558e3a59bd1809f87e9f9
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-07 14:07:46 +0100

    Tuples

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,20 @@ let random = 23834;
 random = "still random";
 random = true;
 let topSecretCodes = [284819, 482849, 290500, 300941];
-// topSecretCodes.push('Abderrahman') // this is a mistake, because array is specified to be filled of numbers.
+/* topSecretCodes.push('Abderrahman') // this is a mistake, because array is specified to be filled of numbers.*/
 topSecretCodes.push(100100010);
 console.log(topSecretCodes);
 let randomArray = [1010, false, "hello"];
+// Tuple
+let data = [
+    1010,
+    "Abderrahman",
+    true,
+]; /* ['Abderrahman', true, 1010] this is wrong*/
+// Tuple array
+let shoppingCart;
+shoppingCart = [
+    [3, "potato"],
+    [1, "soda"],
+    [1, "laptop"],
+];
