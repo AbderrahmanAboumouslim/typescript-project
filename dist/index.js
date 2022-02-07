be10046adfdf2699cb26d1b9b5c3434db4312616
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-07 13:58:59 +0100

    Basic Types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
+// Basic
 let result = 19;
+let companyName = "Aboumouslim Dev";
+let isCompanyFamous = false;
+let random = 23834;
+random = "still random";
+random = true;
+let topSecretCodes = [284819, 482849, 290500, 300941];
+// topSecretCodes.push('Abderrahman') // this is a mistake, because array is specified to be filled of numbers.
+topSecretCodes.push(100100010);
+console.log(topSecretCodes);
+let randomArray = [1010, false, "hello"];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+// Basic
 let result: number = 19;
+let companyName: string = "Aboumouslim Dev";
+let isCompanyFamous: boolean = false;
+let random: any = 23834;
+random = "still random";
+random = true;
+
+let topSecretCodes: number[] = [284819, 482849, 290500, 300941];
+// topSecretCodes.push('Abderrahman') // this is a mistake, because array is specified to be filled of numbers.
+topSecretCodes.push(100100010);
+console.log(topSecretCodes);
+
+let randomArray: any[] = [1010, false, "hello"];
